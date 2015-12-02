1d56a8bd20e0362d1e987570dec5f62ad3036bf0
joonahn <sdeveloper@naver.com>
2015-12-03 01:36:05 +0900

    트롤한거 고침

--- map.h
+++ map.h
@@ -27,15 +27,11 @@ public:
     Character * getCharacter();
     Tile * getTile(int x, int y);
     void placeObject();
-<<<<<<< HEAD
-    void keyboardInput(QKeyEvent * event);
-=======
     SJW * getJunwi(int index);
 public slots:
     void moveall();
     void reset();
 signals:
->>>>>>> 1cfb319dd70592b32ec6526704595312b6648795
 };
 
 #endif // MAP_H
