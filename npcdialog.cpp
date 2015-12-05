8166deb325ab46d037098ecc07194fa80842c09d
joonahn <sdeveloper@naver.com>
2015-12-05 21:32:27 +0900

    image resized and triangle added

--- npcdialog.cpp
+++ npcdialog.cpp
@@ -19,9 +19,10 @@ void Npcdialog::insol1Dialog(int index)
         dialoglabel->setPixmap(*pixmap);
         delete pixmap;
         dialogtextlabel->setText("안녕! 제대로 찾아왔군, 내가 팔이 짧아서 그런데 침대 뒤에 떨어진 내 레포트를 찾아주지 않을래?\n내가 침대를 당길게");
-        dialogselectlabel[0]->setText("네 당연하죠!");
+        dialogselectlabel[0]->setText("▶네 당연하죠!");
         dialogselectlabel[1]->setText("(내키지는 않지만)네...");
         nselect = 2;
+        currentchoice = 0;
     }
     else if(index==2)
     {
@@ -31,14 +32,16 @@ void Npcdialog::insol1Dialog(int index)
     {
         //Succeeded in mission
         dialogtextlabel->setText("훌륭하군! 내 레포트를 찾아준 보답으로 육일콜 번호를 주도록 하지, 아 그리고 밖에 나갈거면 학생증이 필요할거야. 나는 없는데 학생증 가지고 있는 다른 인솔자 방 비밀번호를 알려줄게. " + QString::number(/*map->getPasswd()*/1234) + "잘 외워 두라고!");
-        dialogselectlabel[0]->setText("고맙다고 인사한다.");
+        dialogselectlabel[0]->setText("▶고맙다고 인사한다.");
+        currentchoice = 0;
         nselect = 1;
     }
     else if(index==4)
     {
         //Fail the mission
         dialogtextlabel->setText("안타까워... 다음에 다시 찾아주길 바래");
-        dialogselectlabel[0]->setText("대화 끝내기");
+        dialogselectlabel[0]->setText("▶대화 끝내기");
+        currentchoice = 0;
         nselect = 1;
     }
     else if(index==5)
@@ -51,7 +54,8 @@ void Npcdialog::insol1Dialog(int index)
         delete pixmap;
         //isInsol2Visited() == false
         dialogtextlabel->setText("인솔자 2의 방을 먼저 방문해야 할 것 같다");
-        dialogselectlabel[0]->setText("나가기");
+        dialogselectlabel[0]->setText("▶나가기");
+        currentchoice = 0;
         nselect = 1;
     }
     currentindex = index;
@@ -114,19 +118,22 @@ void Npcdialog::insol2DIalog(int index)
         delete pixmap;
         dialogtextlabel->setText("아 누군가 찾아온다고 했는데 자고있었네... 미안해.");
         dialogselectlabel[1]->setText("괜찮다");
-        dialogselectlabel[0]->setText("왜 잤느냐고 화를 낸다");
+        dialogselectlabel[0]->setText("▶왜 잤느냐고 화를 낸다");
+        currentchoice = 0;
         nselect = 2;
     }
     else if(index==2)
     {
         dialogtextlabel->setText("미안해, 잘못했어..... 나가는 카드를 줄테니 용서해주라...");
-        dialogselectlabel[0]->setText("카드를 받는다");
+        dialogselectlabel[0]->setText("▶카드를 받는다");
+        currentchoice = 0;
         nselect = 1;
     }
     else if(index==3)
     {
         dialogtextlabel->setText("미안... 큭큭 나가는 카드는 여기 있어.");
-        dialogselectlabel[0]->setText("카드를 받는다");
+        dialogselectlabel[0]->setText("▶카드를 받는다");
+        currentchoice = 0;
         nselect = 1;
     }
     currentindex = index;
@@ -168,8 +175,9 @@ void Npcdialog::friendDialog(int index)
         delete pixmap;
         NPCname->setText("친구 1");
         dialogtextlabel->setText("친구가 자고 있다");
-        dialogselectlabel[0]->setText("나가기");
+        dialogselectlabel[0]->setText("▶나가기");
         nselect = 1;
+        currentchoice = 0;
     }
     else if(index==2)
     {
@@ -197,19 +205,22 @@ void Npcdialog::friendDialog(int index)
         }
 
         dialogtextlabel->setText("오 안녕 친구야! 무슨일이니?");
-        dialogselectlabel[0]->setText("술마시러 나가자고 꼬신다");
+        dialogselectlabel[0]->setText("▶술마시러 나가자고 꼬신다");
+        currentchoice = 0;
         nselect = 1;
     }
     else if(index==3)
     {
         dialogtextlabel->setText("그래! 가자. 너 갈때 불러!");
-        dialogselectlabel[0]->setText("대화 끝내기");
+        dialogselectlabel[0]->setText("▶대화 끝내기");
+        currentchoice = 0;
         nselect = 1;
     }
     else if(index==4)
     {
         dialogtextlabel->setText("나 그냥 잘래...");
-        dialogselectlabel[0]->setText("대화 끝내기");
+        dialogselectlabel[0]->setText("▶대화 끝내기");
+        currentchoice = 0;
         nselect = 1;
     }
     currentindex = index;
@@ -251,7 +262,8 @@ void Npcdialog::emptyDialog()
     delete pixmap;
     NPCname->setText("빈 방");
     dialogtextlabel->setText("그냥 빈 방이다.");
-    dialogselectlabel[0]->setText("나가기");
+    dialogselectlabel[0]->setText("▶나가기");
+    currentchoice = 0;
     nselect = 1;
 }
 
@@ -274,7 +286,8 @@ void Npcdialog::trapDialog()
     delete pixmap;
     NPCname->setText("새준위 및 인솔자");
     dialogtextlabel->setText("너 이자식 이시간에 왜 돌아다니는거야!");
-    dialogselectlabel[0]->setText("방으로 강제송환");
+    dialogselectlabel[0]->setText("▶방으로 강제송환");
+    currentchoice = 0;
     nselect = 1;
 }
 
@@ -289,7 +302,7 @@ void Npcdialog::passwdDialog()
     QPixmap * pixmap;
     pixmap = new QPixmap(":/images/doorlock_back.png");
     this->setPixmap(*pixmap);
-    this->move(0,50);
+    this->move(0,25);
     delete pixmap;
     NPCname->setVisible(false);
     NPCimage->setVisible(false);
@@ -320,7 +333,10 @@ void Npcdialog::passwdDialogSelect(int choice)
     {
         //Backspace
         if(inputpw.length()>0)
+        {
+            inputpwlabel[inputpw.length()-1]->setText("");
             inputpw.remove(inputpw.length()-1,1);
+        }
     }
     else if(choice == -2)
     {
@@ -487,6 +503,10 @@ void Npcdialog::keyboardInput(QKeyEvent *event)
     case Qt::Key_Escape:
         closeDialog();
         break;
+    case Qt::Key_Down:
+    case Qt::Key_Up:
+    case Qt::Key_Enter:
+    case Qt::Key_Backspace:
     default:
         event->ignore();
         break;
